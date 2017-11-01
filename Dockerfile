0f197abe866c7c811e42534739148345cb3bded5
Roman Sokolkov <rsokolkov@gmail.com>
2017-11-01 14:25:58 +0100

    Switch to etcd. (#3)
    Grab ips from etcd as Calico randomly grabs the ip for IPIP tunnel.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,13 @@
 FROM alpine:3.6
 
-RUN apk add --no-cache bash
+RUN apk add --no-cache bash curl
+
+# Get etcdctl
+ENV ETCD_VER=v3.2.4
+RUN \
+  cd /tmp && \
+  curl -L https://storage.googleapis.com/etcd/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz | \
+  tar xz -C /usr/local/bin --strip-components=1
 
 COPY docker-entrypoint.sh /docker-entrypoint.sh
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,21 +4,47 @@
 #
 # Upstream issue: https://github.com/projectcalico/calico/issues/1173
 
+# Following variables should be passed:
+# ETCD_ENDPOINTS
+# ETCD_CA_CERT_FILE
+# ETCD_KEY_FILE
+# ETCD_CERT_FILE
+
 trap "exit 0" SIGINT SIGTERM
 
-while true; do
-    # Get all routes destinated to tunl0.
-    # Felix is always using tunl0 as link name.
-    # See https://github.com/projectcalico/felix/blob/master/intdataplane/ipip_mgr.go#L102
-    tunnel_endpoints=$(ip route list | grep 'tunl0' | awk -F/ '{print $1}')
-    echo -e "Discovered following endpoints:\n${tunnel_endpoints}"
+etcdctl_cmd="etcdctl --endpoints ${ETCD_ENDPOINTS} \
+                     --cert-file ${ETCD_CERT_FILE} \
+                     --key-file ${ETCD_KEY_FILE} \
+                     --ca-file ${ETCD_CA_CERT_FILE}"
 
-    # Try to ping every endpoint ip.
-    for IP in ${tunnel_endpoints}; do
-        echo "Pinging ${IP}"
-        ping -c 1 -W 1 ${IP} > /dev/null || echo "Pinging ${IP} failed"
+declare -a TUNNEL_ENDPOINTS
+
+# Get tunnel endpoint ip from Calico etcd.
+function get_tunnel_endpoints {
+    TUNNEL_ENDPOINTS=()
+    for i in `${etcdctl_cmd} ls /calico/v1/host/`; do
+        ip=`${etcdctl_cmd} get ${i}/config/IpInIpTunnelAddr`
+        [ -z "${ip}" ] && continue
+        TUNNEL_ENDPOINTS+=(${ip})
     done
+    echo -e "Discovered following endpoints: ${TUNNEL_ENDPOINTS[@]}"
+}
 
+function ping_tunnel_endpoints {
+    # Try to ping every endpoint ip.
+    for ip in ${TUNNEL_ENDPOINTS[@]}; do
+        echo "Pinging ${ip}"
+        ping -c 1 -W 1 ${ip} > /dev/null || echo "Pinging ${ip} failed"
+    done
+}
+
+# Just fail if we can not access etcd.
+${etcdctl_cmd} cluster-health > /dev/null || exit 1
+
+while true; do
+    # Create tunnel_endpoints array with info from etcd.
+    get_tunnel_endpoints
+    ping_tunnel_endpoints
     sleep 30 &
     wait
 done
